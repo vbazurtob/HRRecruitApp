bdf916f39dacf0ef5dfde7604ff5d5864c72b42c
Voltaire Bazurto <vbazurtob@gmail.com>
2026-01-06 17:42:44 -0500

    fix: fixed logins both logins. WIP. Todo clean

--- src/main/java/org/vbazurtob/HRRecruitApp/conf/AppSecurityConfig.java
+++ src/main/java/org/vbazurtob/HRRecruitApp/conf/AppSecurityConfig.java
@@ -39,85 +39,42 @@ import org.springframework.security.web.SecurityFilterChain;
 /**
  * @author voltaire
  */
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class AppSecurityConfig {
+  public static final String JSESSIONID_COOKIE = "JSESSIONID";
   @Autowired private AppUserDetailsService appUserDetailsService;
 
-//    @Bean
-//    @Order(0)
-//    public SecurityFilterChain publicFilterChain(HttpSecurity http) throws Exception {
-//        http.csrf(csrf -> csrf.disable()) // Example: Disable CSRF
-//                .authorizeHttpRequests(
-//                        auth ->
-//                                auth
-//                                        .antMatchers(ROOT_PAGE).permitAll()
-//                                        .antMatchers(CSS_FOLDER + "**", JS_FOLDER + "**", IMG_FOLDER + "**")
-//                                        .permitAll()
-//                                        .antMatchers(
-//                                                INDEX_PAGE,
-//                                                PUBLIC_CNTROLLER + HOME_PAGE,
-////                                                PUBLIC_CNTROLLER + "/**",
-//                                                "/error/**")
-//                                        .permitAll()
-////                     .antMatchers(JOBS_ADS_MANAGEMENT_CNTROLLER  +
-////                     "/**").hasAuthority(AppUserDetailsService.ROLE_ADMIN)
-////                                        .anyRequest()
-////                                        .authenticated()
-//                        // All other requests require authentication
-//                );
-//        return http.build();
-//    }
-
-
-private void publicFilterChain(HttpSecurity http) throws Exception {
+  private void publicFilterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable()) // Example: Disable CSRF
-            .authorizeHttpRequests(
-                    auth ->
-                            auth
-                                    .antMatchers(ROOT_PAGE).permitAll()
-                                    .antMatchers(CSS_FOLDER + "**", JS_FOLDER + "**", IMG_FOLDER + "**")
-                                    .permitAll()
-                                    .antMatchers(
-                                            INDEX_PAGE,
-
-                                            PUBLIC_CNTROLLER + HOME_PAGE,
-                                            PUBLIC_CNTROLLER + NOT_AUTHORIZED_PAGE,
-
-APPLICANT_LOGIN_URL,
-APPLICANT_POST_LOGIN_URL,
-APPLICANT_LOGOUT_URL,
-
-
-
-HR_LOGIN_URL,
-HR_POST_LOGIN_URL,
-HR_LOGOUT_URL,
-
-PUBLIC_CNTROLLER + "/**",
-
-//                                            PUBLIC_CNTROLLER + APPLICANT_LOGIN_PAGE,
-//                                            ADMIN_CNTROLLER + HR_MEMBER_LOGIN_PAGE,
-//
-//
-//                                            APPLICANT_CV_CNTROLLER + APPLICANT_LOGIN_PAGE,
-//                                            APPLICANT_CV_CNTROLLER + APPLICANT_LOGOUT,
-//
-//                                            JOBS_ADS_MANAGEMENT_CNTROLLER + HR_MEMBER_LOGIN_PAGE,
-//                                            JOBS_ADS_MANAGEMENT_CNTROLLER + HR_MEMBER_LOGOUT,
-
-                                            "/error/**")
-                                    .permitAll()
-//                     .antMatchers(JOBS_ADS_MANAGEMENT_CNTROLLER  +
-//                     "/**").hasAuthority(AppUserDetailsService.ROLE_ADMIN)
-//                                        .anyRequest()
-//                                        .authenticated()
-                    // All other requests require authentication
+        .authorizeHttpRequests(
+            auth ->
+                auth.antMatchers(ROOT_PAGE)
+                    .permitAll()
+                    .antMatchers(CSS_FOLDER + "**", JS_FOLDER + "**", IMG_FOLDER + "**")
+                    .permitAll()
+                    .antMatchers(
+                        INDEX_PAGE,
+                        PUBLIC_CNTROLLER + HOME_PAGE,
+                        PUBLIC_CNTROLLER + NOT_AUTHORIZED_PAGE,
+//                        APPLICANT_LOGIN_URL,
+//                        APPLICANT_POST_LOGIN_URL,
+//                        APPLICANT_LOGOUT_URL,
+//                        HR_LOGIN_URL,
+//                        HR_POST_LOGIN_URL,
+//                        HR_LOGOUT_URL,
+                        PUBLIC_CNTROLLER + "/**",
+                        "/error/**")
+                    .permitAll()
+            //                     .antMatchers(JOBS_ADS_MANAGEMENT_CNTROLLER  +
+            //                     "/**").hasAuthority(AppUserDetailsService.ROLE_ADMIN)
+            //                                        .anyRequest()
+            //                                        .authenticated()
+            // All other requests require authentication
             );
-//    return http;
-}
+    //    return http;
+  }
 
   // Admin
   @Bean
@@ -125,28 +82,35 @@ PUBLIC_CNTROLLER + "/**",
   public SecurityFilterChain adminFilterChain(HttpSecurity http) throws Exception {
     publicFilterChain(http);
     http.csrf(csrf -> csrf.disable()) // Example: Disable CSRF
-        .authorizeHttpRequests(
-            auth -> auth
-//                requestMatchers(new AntPathRequestMatcher(ADMIN_CNTROLLER + HR_MEMBER_LOGIN_PAGE)).permitAll()
-//                .requestMatchers(new AntPathRequestMatcher(PUBLIC_CNTROLLER + APPLICANT_LOGIN_PAGE)).permitAll()
+            .requestMatchers( matchers -> matchers.antMatchers(ADMIN_CNTROLLER + "/**",
+                    JOBS_ADS_MANAGEMENT_CNTROLLER + "/**" ) )
+            .authorizeHttpRequests(auth ->
 
-//                PUBLIC_CNTROLLER + APPLICANT_LOGIN_PAGE,
+                    //                requestMatchers(new AntPathRequestMatcher(ADMIN_CNTROLLER +
+                    // HR_MEMBER_LOGIN_PAGE)).permitAll()
+                    //                .requestMatchers(new AntPathRequestMatcher(PUBLIC_CNTROLLER +
+                    // APPLICANT_LOGIN_PAGE)).permitAll()
 
-                    .antMatchers(
-                            ADMIN_CNTROLLER + HR_MEMBER_LOGIN_PAGE,
-                            JOBS_ADS_MANAGEMENT_CNTROLLER + HR_MEMBER_LOGOUT
-                    ).permitAll()
-//                     .antMatchers(JOBS_ADS_MANAGEMENT_CNTROLLER  +
-//                     "/**").hasAuthority(AppUserDetailsService.ROLE_ADMIN)
-                .anyRequest()
-                .authenticated()
+                    //                PUBLIC_CNTROLLER + APPLICANT_LOGIN_PAGE,
+
+                    //                    .antMatchers(
+                    //                        ADMIN_CNTROLLER + HR_MEMBER_LOGIN_PAGE,
+                    //                        JOBS_ADS_MANAGEMENT_CNTROLLER + HR_MEMBER_LOGOUT)
+                    //                    .permitAll()
+
+                    auth.antMatchers(HR_LOGIN_URL, HR_POST_LOGIN_URL).permitAll()
+                        .antMatchers(HR_LOGOUT_URL).permitAll()
+//                        .antMatchers(JOBS_ADS_MANAGEMENT_CNTROLLER  +
+//                                         "/**").hasAuthority(AppUserDetailsService.ROLE_ADMIN)
+                    .anyRequest()
+                    .authenticated()
             // All other requests require authentication
             )
 
         // <-- Root ant matcher
         .formLogin()
         .loginPage(HR_LOGIN_URL)
-//        .loginProcessingUrl(HR_POST_LOGIN_URL) // <-- post login url must match root ant patter
+        .loginProcessingUrl(HR_POST_LOGIN_URL) // <-- post login url must match root ant
         .failureUrl(HR_LOGIN_URL + "?error")
         .defaultSuccessUrl(JOBS_ADS_MANAGEMENT_CNTROLLER + HR_MEMBER_SUMMARY_PAGE)
         .permitAll()
@@ -154,7 +118,7 @@ PUBLIC_CNTROLLER + "/**",
         .logout()
         .logoutUrl(HR_LOGOUT_URL)
         .logoutSuccessUrl(HR_LOGIN_URL + "?logout") // <-- post login url must match root ant patter
-        .deleteCookies("JSESSIONID")
+        .deleteCookies(JSESSIONID_COOKIE)
         .permitAll()
         .and()
         .exceptionHandling()
@@ -162,45 +126,44 @@ PUBLIC_CNTROLLER + "/**",
         .and()
         .csrf()
         .disable();
-      // Example: Enable HTTP Basic authentication
     return http.build();
   }
 
-    @Bean
-    @Order(2)
-    public SecurityFilterChain userFilterChain(HttpSecurity http) throws Exception {
-//        publicFilterChain(http);
-        http.csrf(csrf -> csrf.disable()) // Example: Disable CSRF
-                .authorizeHttpRequests(
-                        auth ->
-                                auth
-                                        .antMatchers(
-
-                                                PUBLIC_CNTROLLER + "**",
-                                                "/error/**")
-                                        .permitAll() // Example: Public access
-                                        .anyRequest()
-                                        .authenticated()
-                        // All other requests require authentication
-                )
-
-                .formLogin()
-                    .loginPage(APPLICANT_LOGIN_URL)
-//                    .loginProcessingUrl(APPLICANT_POST_LOGIN_URL) // This POST URL has to match with root AntMatcher
-                    .failureUrl(APPLICANT_LOGIN_URL + "?error=Failure when trying to log in")
-                    .defaultSuccessUrl(APPLICANT_CV_CNTROLLER + APPLICANT_SUMMARY_PAGE)
-                    .permitAll()
-                .and()
-                    .logout()
-                    .logoutUrl(APPLICANT_LOGOUT_URL) // <-- logout
-                    .logoutSuccessUrl(APPLICANT_LOGIN_URL + "?logout")
-                    .deleteCookies("JSESSIONID")
+  // Applicant
+  @Bean
+  @Order(2)
+  public SecurityFilterChain userFilterChain(HttpSecurity http) throws Exception {
+    publicFilterChain(http);
+    http.csrf(csrf -> csrf.disable()) // Example: Disable CSRF
+        .authorizeHttpRequests(
+            auth ->
+                auth
+                        .antMatchers(APPLICANT_LOGIN_URL,
+                                APPLICANT_POST_LOGIN_URL,
+                                APPLICANT_LOGOUT_URL).permitAll()
+                    .antMatchers(APPLICANT_CV_CNTROLLER  + "/**", APPLICANT_PROFILE_PAGE + "**" )
+                    .hasAuthority(AppUserDetailsService.ROLE_APPLICANT)
+                    .anyRequest()
+                    .authenticated() // All other requests require authentication
+            )
+        .formLogin()
+        .loginPage(APPLICANT_LOGIN_URL)
+        .loginProcessingUrl(APPLICANT_POST_LOGIN_URL) // Must match the POST URL that is being printed in the template
+            // form
+        .failureUrl(APPLICANT_LOGIN_URL + "?error=Failure when trying to log in")
+        .defaultSuccessUrl(APPLICANT_CV_CNTROLLER + APPLICANT_SUMMARY_PAGE)
+        .permitAll()
+        .and()
+            .logout()
+                .logoutUrl(APPLICANT_LOGOUT_URL) // <-- logout
+                .logoutSuccessUrl(APPLICANT_LOGIN_URL + "?logout")
+                .deleteCookies(JSESSIONID_COOKIE)
                 .permitAll()
-                .and()
-                    .exceptionHandling()
-                    .accessDeniedPage(PUBLIC_CNTROLLER + NOT_AUTHORIZED_PAGE); // Example: Enable HTTP Basic authentication
-        return http.build();
-    }
+        .and()
+            .exceptionHandling()
+            .accessDeniedPage(PUBLIC_CNTROLLER + NOT_AUTHORIZED_PAGE); // Example: Enable HTTP Basic authentication
+    return http.build();
+  }
 
   @Bean
   public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
--- src/main/java/org/vbazurtob/HRRecruitApp/ui/pubweb/PublicController.java
+++ src/main/java/org/vbazurtob/HRRecruitApp/ui/pubweb/PublicController.java
@@ -20,7 +20,6 @@
 
 package org.vbazurtob.HRRecruitApp.ui.pubweb;
 
-
 import static org.vbazurtob.HRRecruitApp.conf.ControllerEndpoints.*;
 
 import javax.validation.Valid;
@@ -39,98 +38,86 @@ import org.vbazurtob.HRRecruitApp.model.service.ApplicantService;
 
 /**
  * @author Voltaire Bazurto Blacio
- * 
- * All rights reserved
- *
- * Controller for all the public pages
- *
+ *     <p>All rights reserved
+ *     <p>Controller for all the public pages
  */
 @Controller
 @RequestMapping(PUBLIC_CNTROLLER)
-public class PublicController implements ControllerEndpoints{
+public class PublicController implements ControllerEndpoints {
 
-	@Autowired
-	private ApplicantService applicantService;
+  @Autowired private ApplicantService applicantService;
 
-	@RequestMapping( { HOME_PAGE })	
-	public String HomepageController(Model model) {
-		
-		String controllerMapping = this.getClass().getAnnotation(RequestMapping.class).value()[0];
-		model.addAttribute("applicantLoginUrl", controllerMapping + APPLICANT_LOGIN_PAGE);
-		model.addAttribute("hrMemberLoginUrl" , HR_LOGIN_URL);
-		return "public/home";
-	}
+  @RequestMapping({HOME_PAGE})
+  public String HomepageController(Model model) {
+    model.addAttribute("applicantLoginUrl", APPLICANT_LOGIN_URL);
+    model.addAttribute("hrMemberLoginUrl", HR_LOGIN_URL);
+    return "public/home";
+  }
 
-	
-	@RequestMapping(APPLICANT_LOGIN_PAGE)	
-	public String LoginController(Model model) {
-		
-		String controllerMapping = this.getClass().getAnnotation(RequestMapping.class).value()[0];
-		model.addAttribute("applicantSignupUrl", controllerMapping + CREATE_NEW_APPLICANT_PAGE );
-		model.addAttribute("postFormUrl",  APPLICANT_POST_LOGIN_URL );
-		
-		return "public/login";
-	}
-	
-	
-	@RequestMapping( CREATE_NEW_APPLICANT_PAGE ) //Create new applicant user
-	public String applicantSignUp(
-			Model model
-			) {
-		
-		String controllerMapping = this.getClass().getAnnotation(RequestMapping.class).value()[0];
-		NewApplicantForm newApplicant = new NewApplicantForm();
-		 
-		model.addAttribute("baseUrl", controllerMapping + CREATE_NEW_APPLICANT_PAGE);
-		model.addAttribute("postFormUrl", controllerMapping + POST_CREATE_NEW_APPLICANT_PAGE);
-		model.addAttribute("loginUrl", controllerMapping + APPLICANT_LOGIN_PAGE);
-		
-		model.addAttribute("newApplicantObj", newApplicant);
-		return "public/applicant_signup.html";
-	}
-	
-	@PostMapping( POST_CREATE_NEW_APPLICANT_PAGE )
-	public String createNewApplicant(
-			@Valid @ModelAttribute("newApplicantObj") NewApplicantForm newApplicantForm,
-			BindingResult results, 
-			RedirectAttributes redirectAttrs,
-			Model model
-			) {
-		
-		String controllerMapping = this.getClass().getAnnotation(RequestMapping.class).value()[0];
-		
-		// Model to redirect to login page
-		model.addAttribute("loginUrl", controllerMapping + APPLICANT_LOGIN_PAGE);
-		
-		
-		if( applicantService.usernameExists(newApplicantForm.getUsername()) ) {
-			results.addError(new ObjectError("UsernameAlreadyExists", "The username already exists! Choose another one"));
-		}
-		
-		if(results.hasErrors()) {
-			
-			model.addAttribute("baseUrl", controllerMapping + CREATE_NEW_APPLICANT_PAGE);
-			model.addAttribute("postFormUrl", controllerMapping + POST_CREATE_NEW_APPLICANT_PAGE);
-			model.addAttribute("newApplicantObj", newApplicantForm);
-			
-			return "public/applicant_signup.html";
-		}
+  @RequestMapping(APPLICANT_LOGIN_PAGE)
+  public String LoginController(Model model) {
+    model.addAttribute("applicantSignupUrl", SIGNUP_NEW_APPLICANT_URL);
+    model.addAttribute("postFormUrl", APPLICANT_POST_LOGIN_URL);
+    return "public/login";
+  }
 
-		applicantService.createNewApplicantInDB(newApplicantForm.getUsername(), newApplicantForm.getPassword(), newApplicantForm.getEmail() );
-		redirectAttrs.addFlashAttribute("saved",true);
-		
-		return "redirect:" + controllerMapping + CREATE_NEW_APPLICANT_PAGE;
-		
-	}
+  @RequestMapping(CREATE_NEW_APPLICANT_PAGE) // Create new applicant user
+  public String applicantSignUp(Model model) {
 
-	@RequestMapping( NOT_AUTHORIZED_PAGE )	
-	public String NotAuthorizedToViewController( Model model ) {
-		
-		return "403";
-	}
+    String controllerMapping = this.getClass().getAnnotation(RequestMapping.class).value()[0];
+    NewApplicantForm newApplicant = new NewApplicantForm();
 
-    @RequestMapping( "/error")
-    public String ErrorController(Model model) {
-        return "error";
+    model.addAttribute("baseUrl", controllerMapping + CREATE_NEW_APPLICANT_PAGE);
+    model.addAttribute("postFormUrl", controllerMapping + POST_CREATE_NEW_APPLICANT_PAGE);
+    model.addAttribute("loginUrl", controllerMapping + APPLICANT_LOGIN_PAGE);
+
+    model.addAttribute("newApplicantObj", newApplicant);
+    return "public/applicant_signup.html";
+  }
+
+  @PostMapping(POST_CREATE_NEW_APPLICANT_PAGE)
+  public String createNewApplicant(
+      @Valid @ModelAttribute("newApplicantObj") NewApplicantForm newApplicantForm,
+      BindingResult results,
+      RedirectAttributes redirectAttrs,
+      Model model) {
+
+    String controllerMapping = this.getClass().getAnnotation(RequestMapping.class).value()[0];
+
+    // Model to redirect to login page
+    model.addAttribute("loginUrl", controllerMapping + APPLICANT_LOGIN_PAGE);
+
+    if (applicantService.usernameExists(newApplicantForm.getUsername())) {
+      results.addError(
+          new ObjectError(
+              "UsernameAlreadyExists", "The username already exists! Choose another one"));
     }
+
+    if (results.hasErrors()) {
+
+      model.addAttribute("baseUrl", controllerMapping + CREATE_NEW_APPLICANT_PAGE);
+      model.addAttribute("postFormUrl", controllerMapping + POST_CREATE_NEW_APPLICANT_PAGE);
+      model.addAttribute("newApplicantObj", newApplicantForm);
+
+      return "public/applicant_signup.html";
+    }
+
+    applicantService.createNewApplicantInDB(
+        newApplicantForm.getUsername(),
+        newApplicantForm.getPassword(),
+        newApplicantForm.getEmail());
+    redirectAttrs.addFlashAttribute("saved", true);
+
+    return "redirect:" + controllerMapping + CREATE_NEW_APPLICANT_PAGE;
+  }
+
+  @RequestMapping(NOT_AUTHORIZED_PAGE)
+  public String NotAuthorizedToViewController(Model model) {
+    return "403";
+  }
+
+  @RequestMapping("/error")
+  public String ErrorController(Model model) {
+    return "error";
+  }
 }
