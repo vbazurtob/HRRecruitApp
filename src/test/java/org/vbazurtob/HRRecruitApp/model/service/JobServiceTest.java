fa90197fcb4da46fe7eff5326e4d2a9bc85d8c8e
Voltaire Bazurto <vbazurtob@gmail.com>
2026-01-12 16:18:35 -0500

    fix: added simple test class for JobService

---
+++ src/test/java/org/vbazurtob/HRRecruitApp/model/service/JobServiceTest.java
@@ -0,0 +1,44 @@
+/*
+ * Personal License Agreement
+ * Copyright Notice
+ *
+ * © 2025 Voltaire Bazurto Blacio. All rights reserved.
+ * License Terms
+ *
+ *     Ownership: All code contained in this portfolio is the sole property of Voltaire Bazurto Blacio and is hereby copyrighted by me.
+ *
+ *     Permitted Use: Others are welcome to view and study the code for personal, educational, or non-commercial purposes. You may share insights or information about the code, but you cannot use it for any commercial products, either as-is or in a derivative form.
+ *
+ *     Restrictions: The code may not be used, reproduced, or distributed for commercial purposes under any circumstances without my explicit written permission.
+ *
+ *     Rights Reserved: I reserve the right to use the code, or any future versions thereof, for my own purposes in any way I choose, including but not limited to the development of future commercial derivative works under my name or personal brand.
+ *
+ *     Disclaimer: The code is provided "as is" without warranty of any kind, either express or implied. I am not responsible for any damages resulting from the use of this code.
+ *
+ * By accessing this portfolio, you agree to abide by these terms.
+ */
+
+package org.vbazurtob.HRRecruitApp.model.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.vbazurtob.HRRecruitApp.model.Job;
+
+public class JobServiceTest {
+
+  @InjectMocks private JobService jobService;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void testValidateFormContent_validateEmptyJobTitle() {
+    Job testJobForm = new Job();
+    String result = jobService.validateFormContent(testJobForm);
+    assert (result).equals("Empty Job Title. It can't be empty.");
+  }
+}
